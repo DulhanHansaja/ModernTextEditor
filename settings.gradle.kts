e66c2ed84e0ccdc8e9b88a50adb6b55b666c810a
DulhanHansaja <ghdulhan.hansaja@gmail.com>
2026-08-16 11:09:09 +0530

    Initial setup: Gradle configuration and dependencies

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+rootProject.name = "TextEditor"
+include(":app")
